ca47e243debdaaf4fd64b9862cdef09b9a7c088f
rochafialvin <rochafi.dev@gmail.com>
2020-06-15 10:22:09 +0700

    new screen

--- src/screens/Add/Add.js
+++ src/screens/Add/Add.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { TouchableOpacity, StyleSheet, Alert, View, Image } from 'react-native'
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text} from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Label, Button, Text, Textarea} from 'native-base';
 import ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import axios from '../../config/api'
-import { btn  } from '../../styles'
+import { btn, bg, m } from '../../styles'
 
 const Add = ({navigation}) => {
 
@@ -15,6 +16,11 @@ const Add = ({navigation}) => {
    const [price, setPrice] = useState('')
    const [description, setDescription] = useState('')
    const [source, setSource] = useState({})
+
+   const changeName = text => setName(text)
+   const changeDescription = text => setDescription(text)
+   const changePrice = text => setPrice(text)
+   const changeStock = text => setStock(text)
 
    const options = {
       title: 'Choose Avatar'
@@ -56,24 +62,36 @@ const Add = ({navigation}) => {
    return (
       <Container>
         <Content>
-            <Form>
-            <Item stackedLabel>
-               <Label>Product Name</Label>
-               <Input value={name} onChangeText={(text) => setName(text)} />
-            </Item>
-            <Item stackedLabel>
-               <Label>Stock</Label>
-               <Input value={stock} onChangeText={(text) => setStock(text)} />
-            </Item>
-            <Item stackedLabel>
-               <Label>Price</Label>
-               <Input value={price} onChangeText={(text) => setPrice(text)} />
-            </Item>
-            <Item stackedLabel last>
-               <Label>Description</Label>
-               <Input value={description} onChangeText={(text) => setDescription(text)} />
-            </Item>
-            </Form>
+            <Text style={styles.label} >Product Name</Text>
+            <Input style={styles.input} placeholder="Insert product name" value={name} onChangeText={changeName}  />
+
+            <Text style={styles.label} >Stock</Text>
+            <Input style={styles.input} placeholder="Insert stock" value={stock} onChangeText={changeStock} />
+
+            <Text style={styles.label} >Price</Text>
+            <View style={{flexDirection: "row", marginHorizontal: 10, height: 35}} >
+            {/* viewPriceIcon */}
+               <View style={{
+                  borderWidth: 1,
+                  borderColor: 'grey',
+                  borderTopLeftRadius: 3,
+                  borderBottomLeftRadius: 3,
+                  borderRightWidth: 0,
+                  backgroundColor: '#eaeaea',
+                  width: '10%'
+               }} >
+               {/* priceIcon */}
+                  <Icon style={{
+                     alignSelf: "center",
+                     paddingVertical: 6
+                  }} name="currency-usd" size={20} />
+               </View>
+               {/* priceInput */}
+               <Input style={[styles.priceInput]} placeholder="Hello" value={price} onChangeText={changePrice} />
+            </View>
+
+            <Text style={styles.label} >Description</Text>
+            <Textarea style={styles.description} placeholder="Your Description" value={description} onChangeText={changeDescription} />
 
             <View style={styles.photo} > 
                <Image style={styles.image} source={source} />
@@ -81,13 +99,13 @@ const Add = ({navigation}) => {
 
             {/* Button choose dan save */}
             <View style={styles.buttons} >
-               <Button style={[btn]} onPress={onChooseImage} >
+               <Button style={[btn, styles.btnChoose]} onPress={onChooseImage} >
                   <Text style={styles.btnText}>Choose</Text>
                </Button>
             </View>
 
 
-            <Button block style={styles.signBtn}  onPress={onAddProduct} >
+            <Button block style={[btn, bg.purple, m.ten]}  onPress={onAddProduct} >
                <Text>Add</Text>
             </Button>
          </Content>
@@ -96,42 +114,60 @@ const Add = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
+   label: {
+      margin : 10,
+      color: "purple"
+   },
+   input: {
+      borderWidth: 1,
+      borderRadius: 3,
+      borderColor: 'grey',
+      marginHorizontal:10,
+      height: 35,
+      fontSize: 15,
+      paddingVertical: 5,
+      paddingLeft: 10
+   },
+   description: {
+      borderWidth: 1,
+      borderRadius: 3,
+      borderColor: 'grey',
+      marginHorizontal:10,
+      height: 90
+   },
+   priceInput: {
+      borderWidth: 1,
+      borderTopRightRadius : 3,
+      borderBottomRightRadius : 3,
+      borderColor: 'grey',
+      height:35,
+      paddingVertical: 5,
+      paddingLeft: 10
+   },
    photo : {
       width: 170,
       height: 170 ,
       borderWidth: 1,
+      borderColor: 'grey',
+      borderRadius: 3,
       margin: 10,
       alignSelf: 'center'
    },
-   image : {
+   image: {
       width: '100%',
       height: '100%'
    },
+   btnChoose: {
+      backgroundColor: '#eaeaea',
+      width: '60%',
+      alignSelf: 'center',
+      height: 30
+   },
    btnText : {
+      color: 'black',
       width: '100%',
-      textAlign: 'center'
-   },
-   text: {
-      marginTop: 35,
-      fontSize: 25,
-      textAlign: 'center'
-   },
-   signBtn : {
-      backgroundColor: 'purple',
-      padding: 9,
-      margin: 5,
-      borderRadius: 5
-   },
-   signText: {
-      fontSize: 19,
-      color: 'white',
-      textAlign: 'center'
-   },
-   signUpText: {
-      marginTop: 5,
-      fontSize: 17,
-      color: 'blue',
-      textAlign: 'center'
+      textAlign: 'center',
+      fontWeight: 'bold'
    }
 })
 
