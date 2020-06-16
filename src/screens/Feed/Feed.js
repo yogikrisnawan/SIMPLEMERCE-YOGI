6955746bfe652f21627215f8fe1886776497cdf1
rochafialvin <rochafi.dev@gmail.com>
2020-06-16 11:48:12 +0700

    Feed

--- src/screens/Feed/Feed.js
+++ src/screens/Feed/Feed.js
@@ -1,24 +1,64 @@
-import React, {useState, useEffect, useCallback} from 'react'
-import {useFocusEffect} from '@react-navigation/native'
-import { View, Text } from 'react-native'
+import React, {useCallback, useState} from 'react'
+import { useSelector }  from 'react-redux'
+import { useFocusEffect } from '@react-navigation/native'
+import axios from '../../config/api'
+import { View, FlatList, TouchableOpacity, Image } from 'react-native'
+import { 
+   Container, Header, Content, Card, CardItem, Thumbnail,
+   Text, Button, Icon, Left, Body, Right
+} from 'native-base';
+
+const FeedProduct = ({item}) => (
+   <Card >
+      <CardItem>
+      <Left>
+         <Thumbnail source={{
+            uri: `http://localhost:2020/user/avatar/${item.usrAvatar}?unq=${new Date()}`}} />
+         <Body>
+            <Text>{item.usrName} </Text>
+            <Text note>Jakarta</Text>
+         </Body>
+      </Left>
+      </CardItem>
+      <CardItem cardBody>
+         <Image source={{
+            uri: `http://localhost:2020/product/picture/${item.picture}?unq=${new Date()}`}} style={{height: 200, width: null, flex: 1}}/>
+      </CardItem>
+      <CardItem>
+         <View>
+            <Text
+            style={{fontSize: 20, fontWeight: 'bold'}}
+            >{item.name} </Text>
+            <Text
+               style={{fontSize: 17, color: 'purple', fontWeight: 'bold'}}
+            >$ {item.price} </Text>
+         </View>
+      </CardItem>
+   </Card>
+)
 
 const Feed = () => {
 
+   const token = useSelector(state => state.auth.token)
    const [products, setproducts] = useState(null)
 
    useFocusEffect(
       useCallback(() => {
-         console.log("FEED COMPONENT")
-         // axios ...
-         // then
-         setproducts([{film: "Daredevil"},{film: "Quite Place"},{film: "Doom"},])
+         const config = {headers: {Authorization : token}}
+         axios.get('/products',  config)
+            .then(res => setproducts(res.data))
+            .catch(err => console.log({err}))
       }, [])
    )
 
    
    return (
-      <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}} >
-         <Text style={{fontSize:30, fontWeight: 'bold'}} >Feed Component</Text>
+      <View >
+         <FlatList
+            data={products}
+            renderItem={({item}) => <FeedProduct item={item} />}
+            keyExtractor={item =>  item.id.toString()}
+         />
       </View>
    )
 }
